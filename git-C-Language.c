53e2d4ebc25bd4f3b40e16585596b4e4037ec7d2
Prince-217 <naiprince345@gmail.com>
2026-01-08 10:10:39 +0530

    Programming in C Language

---
+++ git-C-Language.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b,c;
+	
+	printf("Enter The First Number :- ");
+	scanf("%d",&a);
+	
+	printf("\nEnter The Second Number :- ");
+	scanf("%d",&b);
+	
+	c = a + b;
+	
+	printf("\nThe Sum Of These Two Number Is :- %d",c);
+	
+	c = a * b;
+	
+	printf("\nThe Multiplication Of These Two Number Is :- %d",c);
+	
+	
+}
