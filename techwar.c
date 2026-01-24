8835f23729033e97d18b3d99bd9c0a32c76dba43
Prince S. Nayi <naiprince345@gmail.com>
2026-01-24 15:01:20 +0530

    Add files via upload

---
+++ techwar.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+main()
+
+{
+	int i, j, a, b;
+	
+	for(i=0;i<=10;i++)
+	{
+		for(j=2;j<=i;j++)
+		{
+			
+			
+			if(j%2 != 0)
+			{
+				printf("%d ",j-2);
+			}
+			
+			else
+			{
+				printf("* ");
+			}
+			
+		}
+		printf("\n");
+	}
+	
+	printf("T E C H  W A R  2 0 2 6\n");
+	
+	for(a=11; a>=3; a--)
+	{
+		for(b=3; b<=a; b++)
+		{
+			printf("%d ",b);
+		}
+		printf("\n");
+	}
+}
